1445abba34c00278ccc42f3abb8854b5a624d545
Saurabh Sharma <jawas.saurabh@gmail.com>
2020-01-24 21:41:59 +0530

    completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -23,5 +23,13 @@ gulp.task('watch' , () => {
   watch('app/assets/styles/**/*.css', () => {
     gulp.start('cssInject')
   })
+
+  watch('./app/assets/scripts/**/*.js', () => {
+    gulp.start('scriptsRefresh')
+  })
 });
 
+
+gulp.task('scriptsRefresh', ['scripts'], () => {
+  browserSync.reload()
+})
